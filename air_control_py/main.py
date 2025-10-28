7a4cf9d4297d581c213e503fc6fa7a125af6e5bb
Ivan Morales <ivan.morales9@upr.edu>
2025-10-28 19:25:44 -0400

    Python initial code

--- air_control_py/main.py
+++ air_control_py/main.py
@@ -12,13 +12,21 @@ TOTAL_TAKEOFFS = 20
 STRIPS = 5
 shm_data = []
 
+runway1_lock = threading.Lock()
+runway2_lock = threading.Lock()
+state_lock   = threading.Lock()
+
+SH_MEMORY_NAME = "Shared_Memory_Segment"
 # TODO1: Size of shared memory for 3 integers (current process pid, radio, ground) use ctypes.sizeof()
 # SHM_LENGTH=
+SHM_LENGTH = ctypes.sizeof(ctypes.c_int)*3 #sets size to 3 int
+
 
 # Global variables and locks
 planes = 0  # planes waiting
 takeoffs = 0  # local takeoffs (per thread)
 total_takeoffs = 0  # total takeoffs
+shm_data = None # will hold the memoryview that maps the shared memory
 
 
 
@@ -33,6 +41,24 @@ def create_shared_memory():
     # 6. Use mmap to map the shared memory
     # 7. Create an integer-array view (use memoryview()) to access the shared memory
     # 8. Return the file descriptor (shm_open), mmap object and memory view
+    shm_name = SH_MEMORY_NAME.encode("utf-8")
+    prev_umask = os.umask(0)
+    fd = _libc.shm_open(shm_name,os.O_CREAT|os.O_RDWR,0o666)
+    os.umask(prev_umask)
+    if fd < 0:
+        err = ctypes.get_errno()
+        raise OSError(err, "shm_open failed")
+    
+    _libc.ftruncate(fd,SHM_LENGTH)
+    memory = mmap.mmap(
+        fd,             
+        SHM_LENGTH, 
+        flags=mmap.MAP_SHARED,
+        prot=mmap.PROT_READ | mmap.PROT_WRITE
+    )
+    data = memoryview(memory).cast("i")
+
+    return fd, memory, data
 
 
 
@@ -41,13 +67,14 @@ def HandleUSR2(signum, frame):
     Complete function to update waiting planes"""
     global planes
     # TODO 4: increment the global variable planes
+    planes +=5
     pass
 
 
 def TakeOffFunction(agent_id: int):
     """Function executed by each THREAD to control takeoffs.
     Complete using runway1_lock and runway2_lock and state_lock to synchronize"""
-    global planes, takeoffs, total_takeoffs
+    global planes, takeoffs, total_takeoffs, shm_data
 
     # TODO: implement the logic to control a takeoff thread
     # Use a loop that runs while total_takeoffs < TOTAL_TAKEOFFS
@@ -56,6 +83,72 @@ def TakeOffFunction(agent_id: int):
     # Simulate the time a takeoff takes with sleep(1)
     # Send SIGUSR1 every 5 local takeoffs
     # Send SIGTERM when the total takeoffs target is reached
+    while total_takeoffs < TOTAL_TAKEOFFS:
+         # Try RUNWAY 1 path
+        if runway1_lock.acquire(blocking=False):
+            try:
+                # Try to lock state and check if there's a plane
+                if state_lock.acquire(blocking=False):
+                    try:
+                        if planes > 0:
+                            # plane departs
+                            planes -= 1
+                            takeoffs += 1
+                            total_takeoffs += 1
+
+                            # If we've done 5 takeoffs in this batch, notify radio
+                            if takeoffs == 5:
+                                if shm_data[1] is not None:
+                                    os.kill(shm_data[1], signal.SIGUSR1)
+                                takeoffs = 0  # reset batch
+
+                            # Simulate takeoff time
+                            time.sleep(1)
+
+                            # If we've hit the mission total, tell radio to shut down and exit
+                            if total_takeoffs >= TOTAL_TAKEOFFS:
+                                if shm_data[1] is not None:
+                                    os.kill(shm_data[1], signal.SIGTERM)
+                                return
+
+                        # else: no planes, just fall through
+                    finally:
+                        state_lock.release()
+                else:
+                    # couldn't lock state_lock right now
+                    pass
+            finally:
+                runway1_lock.release()
+
+        elif runway2_lock.acquire(blocking=False):
+            try:
+                if state_lock.acquire(blocking=False):
+                    try:
+                        if planes > 0:
+                            planes -= 1
+                            takeoffs += 1
+                            total_takeoffs += 1
+
+                            if takeoffs == 5:
+                                if shm_data[1] is not None:
+                                    os.kill(shm_data[1], signal.SIGUSR1)
+                                takeoffs = 0
+
+                            time.sleep(1)
+
+                            if total_takeoffs >= TOTAL_TAKEOFFS:
+                                if shm_data[1] is not None:
+                                    os.kill(shm_data[1], signal.SIGTERM)
+                                return
+                    finally:
+                        state_lock.release()
+                else:
+                    pass
+            finally:
+                runway2_lock.release()
+        else:
+            time.sleep(0.001)
+    return
     pass
 
 
@@ -65,21 +158,39 @@ def launch_radio():
         signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGUSR2})
 
     # TODO 8: Launch the external 'radio' process using subprocess.Popen()
-    # process = 
-    # return process
+    process = subprocess.Popen(['./radio',SH_MEMORY_NAME],preexec_fn=_unblock_sigusr2)
+    return process
 
 
 def main():
     global shm_data
 
     # TODO 2: set the handler for the SIGUSR2 signal to HandleUSR2
+    signal.signal(signal.SIGUSR2,HandleUSR2)
     # TODO 5: Create the shared memory and store the current process PID using create_shared_memory()
-    # fd, memory, data = 
+    fd, memory, data =  create_shared_memory()
+    shm_data = data
+    shm_data[0] = os.getpid()
+    
     # TODO 7: Run radio and store its PID in shared memory, use the launch_radio function
-    # radio_process = 
+    radio_process = launch_radio()
+    shm_data[1] = radio_process.pid
     # TODO 9: Create and start takeoff controller threads (STRIPS) 
+    control_threads= []
+    for i in range(STRIPS):
+        thr = threading.Thread(TakeOffFunction,(i,))
+        thr.start()
+        control_threads.append(thr)
+
     # TODO 10: Wait for all threads to finish their work
+    for thr in control_threads:
+            thr.join()
+
     # TODO 11: Release shared memory and close resources
+    data.release()
+    memory.close()
+    os.close(fd)
+    
 
 
 
